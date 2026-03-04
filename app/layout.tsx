e26b08dfbd7ea07f841ce0f90ff0c9ef7f14b086
oudom <oudomphoem@gmail.com>
2026-03-04 20:03:34 +0700

    feat: remove responsivevoice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +28,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Script src="https://code.responsivevoice.org/responsivevoice.js" />
       </body>
     </html>
   );
